5a9d4a80f7f6bfb9092794f704e36239e7fac007
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-10-04 14:16:04 -0400

    commit tree 06228955d09453f2dd34ee92e9e1086b6e6e4b06

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+
+
+
+
+
 // code your solution here
+function superbowlWin(arr){
+  const index = arr.find(element => element.result === 'W');
+  return !!index ? index.year : undefined
+}
+
+
+
